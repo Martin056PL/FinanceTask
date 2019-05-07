0585bf847df89d117443ff47114d829f9a7af18b
Kamil Wawer <kamil.wawer@pollub.edu.pl>
2019-05-07 22:23:25 +0200

    Change Array for List solution

--- src/main/java/wawer/kamil/model/AccountList.java
+++ src/main/java/wawer/kamil/model/AccountList.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
 import java.util.Arrays;
+import java.util.List;
 
 @JacksonXmlRootElement(localName = "accounts")
 public final class AccountList {
@@ -11,26 +12,27 @@ public final class AccountList {
     @JacksonXmlElementWrapper(localName = "account", useWrapping = false)
 
     private Account[] account;
+    private List<Account> accountList;
 
     public AccountList() {
     }
 
-    public AccountList(Account[] employee) {
-        this.account = employee;
+    public AccountList(List<Account> employee) {
+        this.accountList = employee;
     }
 
-    public Account[] getAccount() {
-        return account;
+    public List<Account> getAccount() {
+        return accountList;
     }
 
-    public void setAccount(Account[] account) {
-        this.account = account;
+    public void setAccount(List<Account> account) {
+        this.accountList = account;
     }
 
     @Override
     public String toString() {
         return "Employees{" +
-                "employees=" + Arrays.toString(account) +
+                "employees=" + accountList.toString() +
                 '}';
     }
 }
