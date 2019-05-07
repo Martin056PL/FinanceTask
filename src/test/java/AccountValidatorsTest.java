c4dd1f46b5b7c6c1c277489f446e61336a81d6a8
Kamil Wawer <kamil.wawer@pollub.edu.pl>
2019-05-07 20:33:39 +0200

    Delete broken tests

--- src/test/java/AccountValidatorsTest.java
+++ src/test/java/AccountValidatorsTest.java
@@ -14,47 +14,4 @@ public class AccountValidatorsTest {
         this.account = new Account("PL61109010140000071219812870", "name4", "PLN", BigDecimal.ZERO, LocalDate.parse("2029-10-11"));
     }
 
-    /*@Test
-    public void shouldReturnTrueWithCorrectItIsDefaultData() {
-        boolean isAccountCorrect = validators.checkAllValidators(account);
-        Assert.assertTrue(isAccountCorrect);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldReturnNullPointerExceptionWhenIbanNumberIsNull() {
-        Account account = new Account(null, "Asd", "asda", BigDecimal.ZERO, LocalDate.parse("2010-12-12"));
-
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldReturnNullPointerExceptionWhenAccountNameIsNull() {
-        account.setName(null);
-        System.out.println(account);
-        boolean isAccountCorrect = validators.checkAllValidators(account);
-    }
-    @Test
-    public void shouldReturnFalseWithIncorrectIbanNumber() {
-        boolean isAccountCorrect = validators.checkAllValidators(account);
-        Assert.assertTrue(isAccountCorrect);
-    }
-
-    @Test
-    public void shouldReturnTrueIfCurrencyEqualsPLNInCorrectCase () {
-        boolean isAccountCorrect = validators.isCurrencyEqualsPLN("PLN");
-        Assert.assertTrue(isAccountCorrect);
-    }
-    @Test
-    public void shouldReturnTrueIfCurrencyEqualsPLNInIncorrectCase () {
-        boolean isAccountCorrect = validators.isCurrencyEqualsPLN("pln");
-        Assert.assertFalse(isAccountCorrect);
-    }
-    @Test
-    public void shouldReturnFalseIfCurrencyEqualsOtherCurrencyPrefix () {
-        AccountValidators validators = new AccountValidators();
-        Method method =
-
-        boolean isAccountCorrect = validators.isCurrencyEqualsPLN("USD");
-        Assert.assertFalse(isAccountCorrect);
-    }*/
-
 }
