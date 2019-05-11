9a191cf6bfd78f60c0593682bff326d8b19a90d9
Kamil Wawer <kamil.wawer@pollub.edu.pl>
2019-05-11 22:42:21 +0200

    Next refactore changes in Parser class, refactore in App.class

--- src/main/java/wawer/kamil/App.java
+++ src/main/java/wawer/kamil/App.java
@@ -1,11 +1,13 @@
 package wawer.kamil;
 
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 import wawer.kamil.accountService.AccountService;
 import wawer.kamil.accountService.AccountServiceImpl;
 import wawer.kamil.model.Account;
 import wawer.kamil.model.AccountList;
 import wawer.kamil.model.Parser;
 
+import javax.xml.stream.XMLStreamException;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -13,16 +15,16 @@ import java.util.List;
 public class App {
 
     public static void main(String[] args) throws IOException {
+        App.executeProgram();
+    }
 
-        File inputFile = new File("src/main/resources/SourceData.xml").getAbsoluteFile();
-        File outputFile = new File("src/main/resources/SortedData.xml").getAbsoluteFile();
-
+    private static void executeProgram() throws IOException {
         Parser parser = new Parser();
-        List<Account> accounts = parser.readFromXML(inputFile).getAccountList();
+        List<Account> accounts = parser.readFromXML().getAccountList();
 
         AccountService service = new AccountServiceImpl();
         List<Account> validatedAccounts = service.validateEverySingleAccount(accounts);
         List<Account> sortedAccounts = service.sortValidatedAccountList(validatedAccounts);
-        parser.writeToXML(sortedAccounts, outputFile);
+        parser.writeToXML(sortedAccounts);
     }
 }
--- src/main/java/wawer/kamil/model/Parser.java
+++ src/main/java/wawer/kamil/model/Parser.java
@@ -2,6 +2,7 @@ package wawer.kamil.model;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 
 import java.io.*;
 import java.util.List;
@@ -10,17 +11,21 @@ public class Parser {
 
     private XmlMapper xmlMapper;
 
+    private final File inputFile = new File("src/main/resources/input.xml").getAbsoluteFile();
+    private final File outputFile = new File("src/main/resources/output.xml").getAbsoluteFile();
+
     public Parser() {
         this.xmlMapper = new XmlMapper();
         this.xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
     }
 
-    public AccountList readFromXML(File xmlFile) throws IOException {
-        return xmlMapper.readValue(xmlFile, AccountList.class);
+    public AccountList readFromXML() throws IOException {
+        return xmlMapper.readValue(inputFile, AccountList.class);
     }
 
-    public void writeToXML(List<Account> accounts, File output) throws IOException {
-        xmlMapper.writeValue(output, new AccountList(accounts));
+    public void writeToXML(List<Account> accounts) throws IOException {
+        xmlMapper.configure( ToXmlGenerator.Feature.WRITE_XML_DECLARATION, true );
+        xmlMapper.writeValue(outputFile, new AccountList(accounts));
     }
 
 }
